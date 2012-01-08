079268bfc8eb5c14b7fdf9d0bd74b0e5a4b6c533
luka ramishvili <luka.ramishvili@gmail.com>
2012-01-08 11:24:33 +0400

    dgiuri navachri works

--- Database/StorProcs/DgiuriNavachri.sql
+++ Database/StorProcs/DgiuriNavachri.sql
@@ -27,23 +27,25 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
+    -- jer titoeuli SellOrder-istvis vitvlit velebs (COMPLEX_SO) da shemdeg vajgufebt dgeebis mixedvit
 	SELECT CONVERT(DATE,COMPLEX_SO.dro)
-		 , SUM(COMPLEX_SO.col_1) as col_1
-		 , 0 as col_2
-		 , 0 as col_3
-		 , 0 as col_4
-		 , 0 as col_5
-		 , 0 as col_6
-		 --TODO: other formulas here too
+		 , SUM(COMPLEX_SO.SO_agebuli_tanxa) as sul_agebuli_tanxa
+		 , SUM(COMPLEX_SO.SO_tvitgir) as sul_tvitgir
+		 , SUM(((COMPLEX_SO.SO_agebuli_tanxa-COMPLEX_SO.SO_daubegr_gayid_sum)/1.18)+COMPLEX_SO.SO_daubegr_gayid_sum) as realiz_vat_gareshe
+		 , SUM(((COMPLEX_SO.SO_tvitgir-COMPLEX_SO.SO_daubegr_tvitgir_sum)/1.18)+COMPLEX_SO.SO_daubegr_tvitgir_sum) as tvitgir_vat_gareshe
+		 , SUM(COMPLEX_SO.SO_daubegr_gayid_sum) as sul_daubegr_gayid_sum--col_5
+		 , SUM(COMPLEX_SO.SO_dakarguli) as sul_dakarguli
 	FROM 
 		(SELECT 
 			SO.id as id
 		  , SO.dro as dro
-		  , SUM(Sold.piece_count * Sold.piece_price) as col_1
-		  --TODO: other formulas here
-		FROM SoldRemainders Sold, SellOrder SO
-		WHERE SO.id = Sold.SellOrder_id
+		  , SUM(Sold.piece_count * Sold.piece_price) as SO_agebuli_tanxa--col_1
+		  , SUM(Sold.piece_count * r.buy_price) as SO_tvitgir--col_2
+		  , SUM((CASE WHEN p.uses_vat=0 THEN 1 ELSE 0 END)*Sold.piece_count*r.buy_price) as SO_daubegr_tvitgir_sum
+		  , SUM((CASE WHEN p.uses_vat=0 THEN 1 ELSE 0 END)*Sold.piece_count*Sold.piece_price) as SO_daubegr_gayid_sum--col_5
+		  , SUM(CASE WHEN Sold.piece_price < r.buy_price THEN Sold.piece_count*(r.buy_price - Sold.piece_price) ELSE 0 END) as SO_dakarguli--col_6
+		FROM SoldRemainders Sold, SellOrder SO, remainders r, products p
+		WHERE SO.id = Sold.SellOrder_id AND r.id = Sold.remainder_id AND p.barcode = r.product_barcode
 		GROUP BY SO.id, SO.dro
 		) COMPLEX_SO
 	GROUP BY CONVERT(DATE,COMPLEX_SO.dro)
