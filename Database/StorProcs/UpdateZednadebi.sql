5df64d053c1df8b520eca2b03a4e6dd151b830c6
luka ramishvili <luka.ramishvili@gmail.com>
2012-06-24 13:37:43 +0400

    zed client update working for bought zed

--- Database/StorProcs/UpdateZednadebi.sql
+++ Database/StorProcs/UpdateZednadebi.sql
@@ -49,10 +49,11 @@ BEGIN
 		
 	END
 	
+BEGIN TRANSACTION tran_update_zed
+DECLARE @tran_upd_zed_ret int = 0
+		
 	IF @zed_ident != @new_zed_ident
 	BEGIN
-		BEGIN TRANSACTION tran_update_zed
-		DECLARE @tran_upd_zed_ret int = 0
 		--update zed ident code
 		IF NOT (@new_zed_ident IS NULL) AND NOT (LEN(@new_zed_ident)<1)
 		BEGIN
@@ -80,7 +81,7 @@ BEGIN
 			BEGIN
 				DECLARE @assoc_sold_mt_count int = (SELECT COUNT(id) 
 													FROM money_transfer WHERE client_id = @client_ident 
-																			AND client_type = 'ProductInfo.Supplier'
+																			AND client_type = 'ProductInfo.Buyer'
 																			AND target_ident = @zed_ident)
 				UPDATE zednadebi SET id_code = @new_zed_ident WHERE id_code = @zed_ident AND operation = @operation_type AND client_id = @client_ident
 				IF @@ROWCOUNT < 1 
@@ -96,11 +97,39 @@ BEGIN
 				--
 			END
 		END
-		IF 0 = @tran_upd_zed_ret
-			COMMIT TRANSACTION tran_update_zed
-		ELSE
-			ROLLBACK TRANSACTION tran_update_zed
+	END--end updating zed ident
+	
+	DECLARE @zed_ident_for_upd_client_ident nvarchar(MAX) = @new_zed_ident
+	IF @new_zed_ident IS NULL OR LEN(@new_zed_ident)<1
+		SET @zed_ident_for_upd_client_ident = @zed_ident
+	--update client ident
+	--here, @zed_ident_for_upd_client_ident is not NULL and its length > 1
+	--also, we can safely use @zed_ident_for_upd_client_ident here to find updatable remainders,
+	--because while it's not null and len > 1, then it's either old zed ident (@new_zed_ident equalled @zed_ident), 
+	--or it's a new, valid ident string which is already in the db thanks to upper zed ident updating code
+	IF @new_client_ident IS NOT NULL AND LEN(@new_client_ident)>0
+	BEGIN
+		IF @operation_type = 'Buy'
+		BEGIN
+			UPDATE zednadebi SET client_id = @new_client_ident WHERE id_code = @zed_ident_for_upd_client_ident
+			IF @@ROWCOUNT = 0
+				SET @tran_upd_zed_ret = 500
+			--
+			UPDATE remainders set supplier_ident = @new_client_ident 
+				WHERE zednadebis_nomeri = @zed_ident_for_upd_client_ident AND supplier_ident = @client_ident
+			IF @@ROWCOUNT != @assoc_rem_count 
+				SET @tran_upd_zed_ret = 500
+		END--end updating bought zed's client ident
+		ELSE IF @operation_type = 'Sell'
+		BEGIN
+			SELECT N'TODO update sold zed`s client ident'
+		END
 	END
+	
+	IF 0 = @tran_upd_zed_ret
+		COMMIT TRANSACTION tran_update_zed
+	ELSE
+		ROLLBACK TRANSACTION tran_update_zed
 	
 	RETURN 0
 	
